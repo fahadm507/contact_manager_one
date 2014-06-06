db6df8b35363f2b985bbc915b4a2d72d9a4c20d3
Fahad Mukunzi <fahadm507@gmail.com>
2014-06-06 15:43:44 -0400

    initial commit

---
+++ Rakefile
@@ -0,0 +1,7 @@
+
+#sinatra-activerecord also gives us a bunch of rake tasks that
+#allow us to create our database, add/drop tables, etc. Rake tasks
+#are basically just Ruby scripts that you can run from the command ine.
+# Rakefile
+require "./app"
+require "sinatra/activerecord/rake"
---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'shotgun'
+require "sinatra/activerecord"
+
+require_relative 'models/contact'
+
+
+get '/' do
+  redirect '/contacts'
+end
+
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :index
+end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  erb :show
+end
